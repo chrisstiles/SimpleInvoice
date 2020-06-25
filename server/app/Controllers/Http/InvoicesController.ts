3b316467f30b17ae89017ff1196ec279beb989e8
Chris Stiles <chrisstiles92@gmail.com>
2020-06-25 01:22:27 -0700

    Generate basic PDF

--- server/app/Controllers/Http/InvoicesController.ts
+++ server/app/Controllers/Http/InvoicesController.ts
@@ -1,7 +1,27 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import PDFDocument from 'pdfkit';
+import fs from 'fs';
 
 export default class InvoicesController {
-  public async test() {
-    return 'This is a pdf from a controller';
+  public async test({ response }: HttpContextContract) {
+    // response.implicitEnd = false;
+
+    const doc = new PDFDocument();
+    // const stream = fs.createWriteStream('invoice.pdf');
+
+    doc.pipe(fs.createWriteStream('invoice.pdf'));
+    // doc.pipe(response);
+
+    doc
+      .fontSize(25)
+      .text('This is a PDF generated from a controller');
+
+    doc.end();
+
+    response.header('Content-type', 'application/pdf');
+    doc.pipe(response.response);
+    return response.send(doc);
+    // return pdf;
+    // return await response.download(pdf);
   }
 }
